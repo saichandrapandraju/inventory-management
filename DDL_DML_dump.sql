89bf71a97eb6d3b13f702928de12ee0d3a57a8e0
saichandrapandraju <saichandrapandraju@gmail.com>
2022-12-09 15:02:22 -0500

    included create schema for db dump

--- DDL_DML_dump.sql
+++ DDL_DML_dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `inventory_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `inventory_management`;
 -- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
 --
 -- Host: localhost    Database: inventory_management
@@ -1270,4 +1272,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-09 14:56:21
+-- Dump completed on 2022-12-09 15:01:52
